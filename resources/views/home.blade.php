bbb509d32b3b31c5ccf15f636907647bbd86882c
Alessio1921 <caruso.alessio1999@gmail.com>
2022-05-03 14:35:45 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <title>Laravel Primi Passi</title>
+    <style>
+      html, body {
+          background-color: #fff;
+          color: #636b6f;
+          font-family: 'Nunito', sans-serif;
+          font-weight: 200;
+          height: 100vh;
+          margin: 0;
+      }
+
+      .full-height {
+          height: 100vh;
+      }
+
+      .flex-center {
+          align-items: center;
+          display: flex;
+          justify-content: center;
+      }
+
+      .position-ref {
+          position: relative;
+      }
+
+      .top-right {
+          position: absolute;
+          right: 10px;
+          top: 18px;
+      }
+
+      .content {
+          text-align: center;
+      }
+
+      .title {
+          font-size: 84px;
+      }
+
+      .links > a {
+          color: #636b6f;
+          padding: 0 25px;
+          font-size: 13px;
+          font-weight: 600;
+          letter-spacing: .1rem;
+          text-decoration: none;
+          text-transform: uppercase;
+      }
+
+      .m-b-md {
+          margin-bottom: 30px;
+      }
+  </style>
+  </head>
+  <body>
+    <div class="content">
+      <div class="title m-b-md">
+          <h1>Hello world!!</h1>
+      </div>
+  </body>
+</html>
